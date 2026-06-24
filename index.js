472fbe9215a11e4606f5a20059e928491237a79d
abdul-0987654321 <168588460+abdul-0987654321@users.noreply.github.com>
2026-06-24 13:34:34 +0500

    Update index.js

--- index.js
+++ index.js
@@ -92,14 +92,11 @@ const client = new Client({
   }),
   puppeteer: {
     headless: true,
+    executablePath: process.env.CHROME_PATH || undefined,
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--disable-accelerated-2d-canvas',
-      '--no-first-run',
-      '--no-zygote',
-      '--single-process'
+      '--disable-dev-shm-usage'
     ]
   }
 });
